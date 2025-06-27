2ae62402a4c20513f49dfe97da019c59d599d304
fullstackgenerator <simon.hojnik@gmail.com>
2025-06-27 11:02:36 +0200

    updated companyRegister model

--- Models/CompanyRegister.cs
+++ Models/CompanyRegister.cs
@@ -1,6 +1,50 @@
-﻿namespace TechBoard.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechBoard.Models;
 
 public class CompanyRegister
 {
-    
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength = 5)]
+    [Display(Name = "Company Name")]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [StringLength(30, MinimumLength = 7)]
+    [Display(Name = "Address")]
+    public string Address { get; set; } = null!;
+
+    [Required]
+    [StringLength(10, MinimumLength = 3)]
+    [Display(Name = "Postal Code")]
+    public string PostalCode { get; set; } = null!;
+
+    [Required]
+    [StringLength(20, MinimumLength = 2)]
+    [Display(Name = "City")]
+    public string City { get; set; } = null!;
+
+    [Required]
+    [StringLength(30, MinimumLength = 2)]
+    [Display(Name = "Country")]
+    public string Country { get; set; } = null!;
+
+    [Required]
+    [Phone]
+    [StringLength(20, MinimumLength = 5)]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(50, MinimumLength = 5)]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [StringLength(20, MinimumLength = 5)]
+    [Display(Name = "Business registration number")]
+    public string IdNumber { get; set; } = null!;
 }
