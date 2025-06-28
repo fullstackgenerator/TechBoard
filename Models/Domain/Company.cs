efbbace20108a21dbcc977d29d23baa4221725f7
fullstackgenerator <simon.hojnik@gmail.com>
2025-06-28 17:25:28 +0200

    updated company model and added user model

--- Models/Domain/Company.cs
+++ Models/Domain/Company.cs
@@ -4,14 +4,19 @@ namespace TechBoard.Models.Domain;
 
 public class Company : IdentityUser
 {
-    public string Name { get; set; } = null!;
     public string Address { get; set; } = null!;
     public string PostalCode { get; set; } = null!;
     public string City { get; set; } = null!;
     public string Country { get; set; } = null!;
     public string Phone { get; set; } = null!;
     public string IdNumber { get; set; } = null!;
+    public string? Website { get; set; }
+    public string? Description { get; set; }
+    public int? EmployeeCount { get; set; }
     
-    // navigation property
+    // foreign key
+    public int MembershipTierId { get; set; }
+    
+    public MembershipTier MembershipTier { get; set; } = null!;
     public ICollection<JobPost> JobPosts { get; set; } = new List<JobPost>();
 }
---
+++ Models/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿namespace TechBoard.Models.Domain;
+
+public class User : ApplicationUser
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+    public string? PostalCode { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? LinkedInProfile { get; set; }
+    public string? GitHubProfile { get; set; }
+    public string? Portfolio { get; set; }
+    
+    public ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
+}
